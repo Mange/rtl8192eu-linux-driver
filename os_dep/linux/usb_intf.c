f0c1b5cb1c6aa73bba28250cc67471f0df8dee12
jeremyb31 <jeremy53561@gmail.com>
2016-07-10 08:25:30 -0500

    Update usb_intf.c

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -214,6 +214,7 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
 	{USB_DEVICE_AND_INTERFACE_INFO(USB_VENDER_ID_REALTEK, 0x818C,0xff,0xff,0xff),.driver_info = RTL8192E},/* Default ID */
 	/*=== Customer ID ===*/
 	{USB_DEVICE(0x2001, 0x3319),.driver_info = RTL8192E}, /* D-Link - DWA-131 */
+	{USB_DEVICE(0x2357, 0x0107),.driver_info = RTL8192E}, /* TP-Link - Cameo */
 #endif
 
 #ifdef CONFIG_RTL8723B
