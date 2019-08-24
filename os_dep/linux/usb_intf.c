d02dfec46c11d5b26920d873d8bf2c2ab13fdcf5
vasreb <vasreborn@gmail.com>
2019-08-24 19:11:14 +0300

    Add VID/PID to support Mercusys MW300UH adapter

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -198,6 +198,7 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
 	/*=== Customer ID ===*/
 	{USB_DEVICE(0x2001, 0x3319),.driver_info = RTL8192E}, /* D-Link - DWA-131 */
         {USB_DEVICE(0x2c4e, 0x0100),.driver_info = RTL8192E}, /* Mercusys MW300UM */
+		{USB_DEVICE(0x2c4e, 0x0104),.driver_info = RTL8192E}, /* Mercusys MW300UH */
 #endif
 
 #ifdef CONFIG_RTL8723B
