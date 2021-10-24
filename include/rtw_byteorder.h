1ec7d945eb2e0fa614e9a9e05b65e5ac87ae641e
Carlos Garces <CGarces@users.noreply.github.com>
2021-10-24 13:38:01 +0200

    User byteorder libraries from kernel

--- include/rtw_byteorder.h
+++ include/rtw_byteorder.h
@@ -22,10 +22,10 @@
 
 #if defined(CONFIG_LITTLE_ENDIAN)
 	#ifndef CONFIG_PLATFORM_MSTAR389
-		#include <byteorder/little_endian.h>
+		#include <linux/byteorder/little_endian.h>
 	#endif
 #elif defined (CONFIG_BIG_ENDIAN)
-	#include <byteorder/big_endian.h>
+	#include <linux/byteorder/big_endian.h>
 #else
 	#  error "Must be LITTLE/BIG Endian Host"
 #endif
