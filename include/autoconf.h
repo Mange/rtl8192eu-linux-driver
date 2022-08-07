8027e963b1f287bf886f5b394fa247dc4b08c533
D. Scholz <54322071+dude789@users.noreply.github.com>
2022-08-07 17:54:00 +0200

    Update autoconf.h
    enable CONFIG_RTW_LED

--- include/autoconf.h
+++ include/autoconf.h
@@ -149,7 +149,7 @@
 
 #define CONFIG_SKB_COPY	/* for amsdu */
 
-/* #define CONFIG_RTW_LED */
+#define CONFIG_RTW_LED
 #ifdef CONFIG_RTW_LED
 	/* #define CONFIG_RTW_SW_LED */
 	#ifdef CONFIG_RTW_SW_LED
